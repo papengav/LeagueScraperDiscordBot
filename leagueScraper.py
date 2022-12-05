a053a51de101e42c50bbe870e3c877f414ef5ddf
papengav <35815544+papengav@users.noreply.github.com>
2022-12-05 09:59:03 -0600

    Update leagueScraper.py
    refactor: added decorator method "command" to eliminate recursive code
    
    Methods getRank, getLevel, getWinRate contained recursive code for region validation, Riot API requests, and request validation. This code was refactored into a new "command" method which then passes the summoner object off to the desired function. getRank, getLevel, and getWinRate now only handle extracting the desired data from the summoner object, formatting, and returning a string.

--- leagueScraper.py
+++ leagueScraper.py
@@ -1,12 +1,11 @@
 import requests
 import json
-from enum import Enum
 import RequestExceptions
 import os
 from dotenv import load_dotenv
 
 #Dev Key for Riot API
-#Retrieved from env and initialized in main
+#Retrieved from env and initialized in init
 apiKey = None
 
 #regions stores valid regions invalidRegion stores static error message for when an invalid region is inputted, probably change this later
@@ -21,8 +20,6 @@ def init(key):
     apiKey = key
     regions = ["br1", "eun1", "euw1", "jp1", "kr", "la1", "la2", "na1", "oc1", "tr1", "ru"]
     invalidRegion= "Please submit a valid region", *regions
-
-
 
 #Class to store summoner information
 class Summoner:
@@ -175,7 +172,28 @@ def getSummoner(summonerV4, leagueV4):
 
     return summoner
 
-def getRank(name, region):
+def getRank(summoner):
+    return "{name} is {tier} {rank} with {lp} lp!".format(
+    name = summoner.name,
+    tier = summoner.tier,
+    rank = summoner.rank,
+    lp = summoner.lp)
+
+def getWinRate(summoner):
+    winRate = round(100 * (summoner.wins / (summoner.wins + summoner.losses)))
+
+    return "{name} has {wins} wins and {losses} losses, giving them a winrate of {wr}%".format(
+    name = summoner.name,
+    wins = summoner.wins,
+    losses = summoner.losses,
+    wr = winRate)
+
+def getLevel(summoner):
+    return "{name} is level {level}.".format(
+    name = summoner.name,
+    level = summoner.level)
+
+def command(name, region, func):
     regionIsValid = validateRegion(region)
     summonerV4Request = None
     response = None
@@ -188,61 +206,7 @@ def getRank(name, region):
             leagueV4Request = convertToJson(leagueV4(summonerV4Request, region))
             summoner = getSummoner(summonerV4Request, leagueV4Request)
 
-            response =  "{name} is {tier} {rank} with {lp} lp!".format(
-            name = summoner.name,
-            tier = summoner.tier,
-            rank = summoner.rank,
-            lp = summoner.lp)
-        else:
-            response = errorHandler(summonerV4Request.status_code)
-    else: 
-        response = invalidRegion
-
-    return response
-
-def getWinRate(name, region):
-    regionIsValid = validateRegion(region)
-    summonerV4Request = None
-    response = None
-
-    if regionIsValid:
-        summonerV4Request = summonerV4(name, region)
-
-        if summonerV4Request.status_code == 200:
-            summonerV4Request = convertToJson(summonerV4Request)
-            
-            leagueV4Request = convertToJson(leagueV4(summonerV4Request, region))
-            summoner = getSummoner(summonerV4Request, leagueV4Request)
-            winRate = round(100 * (summoner.wins / (summoner.wins + summoner.losses)))
-
-            response = "{name} has {wins} wins and {losses} losses, giving them a winrate of {wr}%".format(
-            name = summoner.name,
-            wins = summoner.wins,
-            losses = summoner.losses,
-            wr = winRate)
-        else:
-            response = errorHandler(summonerV4Request.status_code)
-    else: 
-        response = invalidRegion
-
-    return response
-
-def getLevel(name, region):
-    regionIsValid = validateRegion(region)
-    summonerV4Request = None
-    response = None
-
-    if regionIsValid:
-        summonerV4Request = summonerV4(name, region)
-
-        if summonerV4Request.status_code == 200:
-            summonerV4Request = convertToJson(summonerV4Request)
-            leagueV4Request = convertToJson(leagueV4(summonerV4Request, region))
-            summoner = getSummoner(summonerV4Request, leagueV4Request)
-
-            response = "{name} is level {level}.".format(
-            name = summoner.name,
-            level = summoner.level)
+            response = func(summoner)
         else:
             response = errorHandler(summonerV4Request.status_code)
     else: 
